66ca941f0cf3966b215af190d105e21551a0e106
Amber J <amber@electri.dev>
2023-03-19 19:25:41 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # polybar_music
-Shell script using playerctl to show music status in Polybar
+Shell script using playerctl to show music status in Polybar.
+Dependencies: polybar, playerctl
+"--player=spotify" can be removed to allow for any player, or replaced with a specific player.
+"man playerctl" is your friend if you want to edit :)
+
+Place this in ~/.config/polybar/config.ini:
+
+[module/music]
+type=custom/script
+exec=/path/to/script
+interval=1
+
+modules-center = music
+
+"interval" updates the module every $interval seconds.
+Make sure the script has execute permissions.
---
+++ music.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+##############################
+#							 #
+# 2023 | https://electri.dev #
+#							 #
+##############################
+
+player_status=$(playerctl --player=spotify status)
+if [ "$player_status" = "Playing" ]; then
+  np=$(playerctl --player=spotify metadata --format "{{ title }} - {{ artist }} - {{ album }}")
+  position=$(playerctl --player=spotify metadata --format '{{ position }}')
+  position_minutes=$((position/1000000/60))
+  position_seconds=$(printf "%02d" $((position/1000000%60)))
+  echo "$np - $position_minutes:$position_seconds"
+else
+  echo "~"
+fi
